500cbf8fbe35742818ab3f0a588c393653469dbf
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-02-23 22:36:58 +0300

    work perfectly without server

---
+++ src/services/book.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+const baseUrl = 'https://www.googleapis.com/books/v1/volumes?q='
+const apiKey = process.env.REACT_APP_GOOGLE_API_KEY
+
+export default async function getBooks ( title ) {
+    const response = await axios.get( `${baseUrl}intitle:${title}&key=${apiKey}` )
+    return response.data
+}
+
+// export default { getBooks }
