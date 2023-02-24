b110e8fa41a85ebf825ff7b7d9c3cc78a6fbfed1
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-02-25 01:19:20 +0300

    prepare for deploy

--- src/services/book.js
+++ src/services/book.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:8000/api/books/'
-
+const baseUrl = '/api/books/'
 
 export default async function getBooks ( title ) {
     const response = await axios.get( `${baseUrl}${title}` )
